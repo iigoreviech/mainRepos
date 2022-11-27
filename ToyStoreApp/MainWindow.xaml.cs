7db52ca3446fb0ed3a06c635429b6915e9ed0703
Igor <igorkrutto@gmail.com>
2022-11-27 17:03:16 +0300

    mainRepos
    mainRepos

---
+++ ToyStoreApp/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ToyStoreApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Log_Click(object sender, RoutedEventArgs e)
+        {
+            string login = textBoxLogin.Text.Trim();
+            string pass = passBox.Password.Trim();
+
+            if(login.Length < 5)
+            {
+                textBoxLogin.ToolTip = "Это поле введено некорректно!";
+                textBoxLogin.Background = Brushes.DarkRed;
+            }
+            else if (pass.Length < 5)
+            {
+                passBox.ToolTip = "Это поле введено некорректно!";
+                passBox.Background = Brushes.DarkRed;
+            }
+            else
+            {
+                textBoxLogin.ToolTip = string.Empty;
+                textBoxLogin.Background = Brushes.Transparent;
+                passBox.ToolTip = string.Empty;
+                passBox.Background = Brushes.Transparent;
+
+                MenuWindow f4 = new MenuWindow();
+                f4.Show();
+                this.Close();
+            }
+
+        }
+    }
+}
